14d6c59ae46f5d8a982b9c193abf28c35643af4d
dahuenecke <dhuenecke@outlook.com>
2019-01-26 11:03:03 -0600

    refactored-to-handle-multiple-recipients

---
+++ Mailman/IEmailConfig.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Mailman 
+{
+    public interface IEmailConfig
+    {
+        string FromName { get; set; }
+        string FromAddress { get; set; }
+        string LocalDomain { get; set; }
+        string MailServerAddress { get; set; }
+        string MailServerPort { get; set; }
+        string UserId { get; set; }
+        string UserPassword { get; set; }
+    }
+}
