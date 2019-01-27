1eda7db5fd4fc32cb4b63be1fdd14449b44ca03b
dahuenecke <dhuenecke@outlook.com>
2019-01-27 07:43:58 -0600

    added ServiceCollection extensions

--- Mailman/Extensions/MailmanExtensions.cs
+++ Mailman/Extensions/MailmanExtensions.cs
@@ -5,9 +5,21 @@ namespace Mailman.Extensions
 {
     public static class EmailExtensions
     {
-        public static IServiceCollection AddMailman(this IServiceCollection services, IEmailConfig config)
+        public static IServiceCollection AddMailman(this IServiceCollection services, IEmailConfig configuration)
         {
-            throw new NotImplementedException();
+            if (services == null) throw new ArgumentNullException(nameof(services));
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+
+            return services.AddTransient<IEmailService>(provider => new EmailService(configuration));
+        }
+
+        public static IServiceCollection AddMailman(this IServiceCollection services, Func<IEmailConfig> configuration)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+
+            IEmailConfig config = configuration();
+            return services.AddTransient<IEmailService>(provider => new EmailService(config));
         }
     }
 }
