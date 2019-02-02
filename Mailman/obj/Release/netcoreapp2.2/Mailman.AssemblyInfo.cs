8207536951f253b3199cb16cdfd0eebe5a4464eb
dahuenecke <dhuenecke@outlook.com>
2019-02-02 11:54:55 -0600

    added nuget package info

--- Mailman/obj/Release/netcoreapp2.2/Mailman.AssemblyInfo.cs
+++ Mailman/obj/Release/netcoreapp2.2/Mailman.AssemblyInfo.cs
@@ -11,8 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Dan Huenecke")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Mailman")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mailman")]
